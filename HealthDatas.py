e2ce1c2c781c29e20a4775339ce7e1429923d186
Sander Rodrigues <155313547+sander-dallorto@users.noreply.github.com>
2024-10-26 10:41:30 -0300

    Add files via upload

--- HealthDatas.py
+++ HealthDatas.py
@@ -10,64 +10,23 @@ from pydantic import BaseModel, confloat, conint
 from typing import Optional, Literal
 
 class HealthData(BaseModel):
-    sex: Optional[confloat(ge=0, le=1)]  
-    age: Optional[float]
-    height: Optional[float]
-    weight: Optional[float]
-    waistline: Optional[float]
-    sight_left: Optional[float]
-    sight_right: Optional[float]
-    hear_left: Optional[float]
-    hear_right: Optional[float]
-    SBP: Optional[float]  # Systolic Blood Pressure
-    DBP: Optional[float]  # Diastolic Blood Pressure
-    BLDS: Optional[float]  # Blood Sugar
-    tot_chole: Optional[float]  # Total Cholesterol
-    HDL_chole: Optional[float]  # HDL Cholesterol
-    LDL_chole: Optional[float]  # LDL Cholesterol
-    triglyceride: Optional[float]
-    hemoglobin: Optional[float]
-    urine_protein: Optional[float]
-    serum_creatinine: Optional[float]
-    SGOT_AST: Optional[float]  # AST (Aspartate Aminotransferase)
-    SGOT_ALT: Optional[float]  # ALT (Alanine Aminotransferase)
-    gamma_GTP: Optional[float]
-    SMK_stat_type_cd: Optional[float]  # Smoking Status
-    DRK_YN: Optional[Literal['Y', 'N']]  # Drinking (Yes/No)
-    age_group: Optional[Literal['0-18', '19-35', '36-50', '51-65', '65+']] 
-    bmi: Optional[float]  # Body Mass Index
-    BP_ratio: Optional[float]  # Blood Pressure ratio
-    HDL_ratio: Optional[float]  # HDL Cholesterol ratio
-    LDL_ratio: Optional[float]  # LDL Cholesterol ratio
-    High_Blood_Pressure: Optional[float]
-    High_Total_Cholesterol: Optional[float]
-    High_LDL_Cholesterol: Optional[float]
-    age_group_encoded: Optional[conint(ge=0)] 
-
-    class Config:
-        # Exemplo para realizar predição
-        schema_extra = {
-            "example": {
-                "sex": 1.0,
-                "age": 45.0,
-                "height": 170.0,
-                "weight": 75.0,
-                "waistline": 90.0,
-                "sight_left": 1.0,
-                "sight_right": 1.0,
-                "hear_left": 0.5,
-                "hear_right": 0.5,
-                "SBP": 120.0,
-                "DBP": 80.0,
-                "BLDS": 90.0,
-                "tot_chole": 200.0,
-                "HDL_chole": 50.0,
-                "LDL_chole": 100.0,
-                "triglyceride": 150.0,
-                "hemoglobin": 13.5,
-                "urine_protein": 0.0,
-                "serum_creatinine": 1.0,
-                "SGOT_AST": 30.0,
-                "SGOT_ALT": 25.0,
-                "gamma_GTP": 20.0,}}
+    sex: int
+    age_group: int
+    bmi: float
+    waistline: float
+    sight_left: float
+    sight_right: float
+    hear_left: float
+    hear_right: float
+    triglyceride: float
+    hemoglobin: float
+    urine_protein: float
+    serum_creatinine: float
+    sgot_ast: float
+    sgot_alt: float
+    gamma_gtp: float
+    smk_stat_type_cd: int
+    bp_ratio: float
+    hdl_ratio: float
+    ldl_ratio: float
 
--- app.py
+++ app.py
@@ -5,20 +5,21 @@ Created on Mon Oct 21 14:08:01 2024
 @author: sande
 """
 
-# 1. Importando libs
+# 1. Library imports
 import uvicorn
 from fastapi import FastAPI
-from HealthDatas import HealthData
+from pydantic import BaseModel  
 import numpy as np
 import pickle
 import pandas as pd
 import nest_asyncio
-# 2. Criando o objeto app
-app = FastAPI()
-pickle_in = open("classifier.pkl","rb")
-classifier=pickle.load(pickle_in)
-nest_asyncio.apply()
+from HealthDatas import HealthData
 
+# 2. Create the app object
+app = FastAPI()
+pickle_in = open("classifier.pkl", "rb")
+classifier = pickle.load(pickle_in)
+nest_asyncio.apply()
 
 # 3. Index route, opens automatically on http://127.0.0.1:8000
 @app.get('/')
@@ -26,71 +27,55 @@ def index():
     return {'message': 'Hello, World'}
 
 # 4. Route with a single parameter, returns the parameter within a message
-#    Located at: http://127.0.0.1:8000/AnyNameHere
 @app.get('/{name}')
 def get_name(name: str):
-    return {'Welcome To Krish Youtube Channel': f'{name}'}
+    return {'Bem Vindo ao FastAPI': f'{name}'}
 
-# 3. Expose the prediction functionality, make a prediction from the passed
-#    JSON data and return the predicted Bank Note with the confidence
+# 5. Expose the prediction functionality
 @app.post('/predict')
 def predict_healthdata(data: HealthData):
     # Convertendo os dados para dicionário
-    data = data.dict()
+    data_dict = data.dict()
     
-    # Extraindo os dados relevantes
-    sex = data['sex']
-    age = data['age']
-    height = data['height']
-    weight = data['weight']
-    waistline = data['waistline']
-    sight_left = data['sight_left']
-    sight_right = data['sight_right']
-    hear_left = data['hear_left']
-    hear_right = data['hear_right']
-    SBP = data['SBP']
-    DBP = data['DBP']
-    BLDS = data['BLDS']
-    tot_chole = data['tot_chole']
-    HDL_chole = data['HDL_chole']
-    LDL_chole = data['LDL_chole']
-    triglyceride = data['triglyceride']
-    hemoglobin = data['hemoglobin']
-    urine_protein = data['urine_protein']
-    serum_creatinine = data['serum_creatinine']
-    SGOT_AST = data['SGOT_AST']
-    SGOT_ALT = data['SGOT_ALT']
-    gamma_GTP = data['gamma_GTP']
-    SMK_stat_type_cd = data['SMK_stat_type_cd']
-    DRK_YN = 1 if data['DRK_YN'] == 'Y' else 0  # Convertendo DRK_YN para 1 ou 0
-    age_group_encoded = data['age_group_encoded']
-    bmi = data['bmi']
-    BP_ratio = data['BP_ratio']
-    HDL_ratio = data['HDL_ratio']
-    LDL_ratio = data['LDL_ratio']
-    High_Blood_Pressure = data['High_Blood_Pressure']
-    High_Total_Cholesterol = data['High_Total_Cholesterol']
-    High_LDL_Cholesterol = data['High_LDL_Cholesterol']
-    print("Hello!")
-    # Fazendo a previsão
-    prediction = classifier.predict([[age, height, weight, waistline, sight_left, sight_right,
-                                      hear_left, hear_right, SBP, DBP, BLDS, tot_chole,
-                                      HDL_chole, LDL_chole, triglyceride, hemoglobin,
-                                      urine_protein, serum_creatinine, SGOT_AST, SGOT_ALT,
-                                      gamma_GTP, SMK_stat_type_cd, DRK_YN]])
+    # Extracting relevant data
+    features = [
+        data_dict['sex'],
+        data_dict['age_group'],
+        data_dict['bmi'],
+        data_dict['waistline'],
+        data_dict['sight_left'],
+        data_dict['sight_right'],
+        data_dict['hear_left'],
+        data_dict['hear_right'],
+        data_dict['triglyceride'],
+        data_dict['hemoglobin'],
+        data_dict['urine_protein'],
+        data_dict['serum_creatinine'],
+        data_dict['sgot_ast'],
+        data_dict['sgot_alt'],
+        data_dict['gamma_gtp'],
+        data_dict['smk_stat_type_cd'],
+        data_dict['bp_ratio'],
+        data_dict['hdl_ratio'],
+        data_dict['ldl_ratio']
+    ]
     
+    print("Dados recebidos para predição:", data_dict)
+
+    # Making the prediction
+    prediction_proba = classifier.predict_proba([features])[0]
+
     # Interpretando a previsão
-    if prediction[0] > 0.5:
-        result = "Risk of Health Issues"
+    if prediction_proba[1] > 0.5:  # Assuming the second index is for the risk class
+        result = "Risco de Sáude"
     else:
-        result = "Healthy"
+        result = "Saudável"
 
     return {
-        'prediction': prediction
+        'prediction': prediction_proba[1],  # Probability of the health risk
+        'result': result
     }
-# 5. Run the API with uvicorn
-#    Will run on http://127.0.0.1:8000
+
+# 6. Run the API with uvicorn
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
-    
-#uvicorn app:app --reload
